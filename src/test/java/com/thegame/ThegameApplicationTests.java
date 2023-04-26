1704986e3dda3aa29c724fb1b397d48f0e696374
thomasdroege <ThomasDroege@gmx.de>
2023-04-26 19:36:42 +0200

    init commit

---
+++ src/test/java/com/thegame/ThegameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThegameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
