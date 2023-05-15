046f36c424126e69d8e951f2c5357a9143172b41
thomasdroege <ThomasDroege@gmx.de>
2023-05-15 20:46:08 +0200

    Refakturierung village.js/html

--- src/main/resources/static/js/village.js
+++ src/main/resources/static/js/village.js
@@ -10,19 +10,19 @@ var wood = resources.filter(res => res.resourceName == 'Wood')[0];
 var secsDiffWoodTime = (new Date(timestampNow) -  new Date(wood.updateTime))/millisecsPerSec;
 var woodActual = wood.resourceAtUpdateTime + secsDiffWoodTime*wood.resourceIncome/secsPerHour;
 var woodIncome = wood.resourceIncome/secsPerHour;
-var woodLabel = document.getElementById("woodLabelId");
+var woodVal = document.getElementById("woodValId");
 
 var stone = resources.filter(res => res.resourceName == 'Stone')[0]
 var secsDiffStoneTime = (new Date(timestampNow) -  new Date(stone.updateTime))/millisecsPerSec;
 var stoneActual = stone.resourceAtUpdateTime + secsDiffStoneTime*stone.resourceIncome/secsPerHour;
 var stoneIncome = stone.resourceIncome/secsPerHour;
-var stoneLabel = document.getElementById("stoneLabelId");
+var stoneVal = document.getElementById("stoneValId");
 
 var iron = resources.filter(res => res.resourceName == 'Iron')[0];
 var secsDiffIronTime = (new Date(timestampNow) -  new Date(iron.updateTime))/millisecsPerSec;
 var ironActual = iron.resourceAtUpdateTime + secsDiffIronTime*iron.resourceIncome/secsPerHour;
 var ironIncome = iron.resourceIncome/secsPerHour;
-var ironLabel = document.getElementById("ironLabelId");
+var ironVal = document.getElementById("ironValId");
 
 
 setInterval(setResources, millisecsPerSec);
@@ -31,9 +31,9 @@ function setResources() {
     foodActual = foodActual + foodIncome;
     foodVal.innerHTML = (Math.round(foodActual*100)/100).toFixed(0);
     woodActual = woodActual + woodIncome;
-    woodLabel.innerHTML = (Math.round(woodActual*100)/100).toFixed(0);
+    woodVal.innerHTML = (Math.round(woodActual*100)/100).toFixed(0);
     stoneActual = stoneActual + stoneIncome;
-    stoneLabel.innerHTML = (Math.round(stoneActual*100)/100).toFixed(0);
+    stoneVal.innerHTML = (Math.round(stoneActual*100)/100).toFixed(0);
     ironActual = ironActual + ironIncome;
-    ironLabel.innerHTML = (Math.round(ironActual*100)/100).toFixed(0);
+    ironVal.innerHTML = (Math.round(ironActual*100)/100).toFixed(0);
     }
