4e47de8bf26ec7c7d74d9ae41fcb6a677839d959
ThomasDroege <thomasdroege@gmx.de>
2025-06-22 13:32:10 +0200

    test commit

--- src/main/java/com/thegame/business/service/BuildingService.java
+++ src/main/java/com/thegame/business/service/BuildingService.java
@@ -60,7 +60,6 @@ public class BuildingService {
         //großes ToDo: kein Aggregieren von RessZeilen sondern Update von einer RessZeile
         // ToDo: Integrationstests
 
-
         //ToDo: Nutzen der buildinglevel Tabellen
         //ToDo: Auslagern in separaten BuildingLevelService
         //ToDo: Schreiben von Unit Tests
